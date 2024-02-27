5bec9ca152f65e21d6c7f2b3f9e3f640e15c408e
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-02-27 12:06:40 +0200

    Add files via upload

---
+++ assets/servies/config.js
@@ -0,0 +1,21 @@
+const config = {
+    whatsappPhoneNumber: "2799999999",
+    services: [
+        {
+            name: "Instagram Followers ( 10k )",
+            description: "Get real followers on Instagram.",
+            price: "R10"
+        },
+        {
+            name: "Facebook Likes",
+            description: "Increase your Facebook page likes.",
+            price: "R15"
+        },
+        {
+            name: "Twitter Retweets",
+            description: "Boost your tweets with retweets.",
+            price: "R5"
+        },
+        
+    ]
+};
