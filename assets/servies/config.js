bf7e2ba32972de6397c3e24a53b8f807d687c636
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-03-02 13:10:04 +0200

    Update config.js

--- assets/servies/config.js
+++ assets/servies/config.js
@@ -2,19 +2,14 @@ const config = {
     whatsappPhoneNumber: "855319762326",
     services: [
         {
-            name: "Instagram Followers ( 1.5k )",
-            description: "Get real followers on Instagram.",
-            price: "R10"
-        },
-        {
-            name: "Facebook Likes",
-            description: "Increase your Facebook page likes.",
+            name: "Instagram Likes ( 1.5k )",
+            description: "Get real Likes on Instagram.",
             price: "R15"
         },
         {
-            name: "Twitter Retweets",
-            description: "Boost your tweets with retweets.",
-            price: "R5"
+            name: "Tiktok Views Free ( 3k )",
+            description: "Get free Tiktok Views With My Whatsaap Bot",
+            price: "R0"
         },
         
     ]
