e5a09026da0ab934451731a4da757254d7ac5409
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-03-03 12:23:08 +0200

    Update config.js

--- assets/servies/config.js
+++ assets/servies/config.js
@@ -7,6 +7,11 @@ const config = {
             price: "R15"
         },
         {
+            name: "Instagram Likes ( 10k )",
+            description: "Get real Likes on Instagram.",
+            price: "R100"
+        },
+        {
             name: "Tiktok Views Free ( 3k )",
             description: "Get free Tiktok Views With My Whatsaap Bot",
             price: "R0"
