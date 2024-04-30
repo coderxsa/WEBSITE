58a82c37e10004b07494d6cb6d4bd328b6910c7b
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-04-30 12:56:29 +0200

    Update config.js

--- assets/servies/config.js
+++ assets/servies/config.js
@@ -1,27 +1,55 @@
 const config = {
     whatsappPhoneNumber: "+27717311486",
+    platforms: [
+        {
+            name: "Instagram",
+            value: "Instagram"
+        },
+        {
+            name: "TikTok",
+            value: "TikTok"
+        },
+        {
+            name: "Twitter",
+            value: "Twitter"
+        }
+    ],
     services: [
         {
-            name: "Free Trial ( limited amount )",
-            description: "Send me a message",
-            price: "R0"
+            platform: "Instagram",
+            plans: [
+                {
+                    name: "Instagram Likes (1.5k)",
+                    description: "Get real Likes on Instagram.",
+                    price: "R15"
+                },
+                {
+                    name: "Instagram Likes (10k)",
+                    description: "Get real Likes on Instagram.",
+                    price: "R100"
+                },
+                {
+                    name: "Instagram Followers (500)",
+                    description: "Get Followers on Instagram. Low Drop.",
+                    price: "R20"
+                }
+            ]
         },
         {
-            name: "Instagram Likes ( 1.5k )",
-            description: "Get real Likes on Instagram.",
-            price: "R15"
+            platform: "TikTok",
+            plans: [
+                {
+                    name: "TikTok Views ( Free )",
+                    description: "Boost your TikTok views. Order List",
+                    price: "R0"
+                },
+                {
+                    name: "TikTok Likes (1k)",
+                    description: "Increase your TikTok likes.",
+                    price: "R30"
+                }
+            ]
         },
-        {
-            name: "Instagram Likes ( 10k )",
-            description: "Get real Likes on Instagram.",
-            price: "R100"
-        },
-        {
-            name: "Instagram Followers ( 500 )",
-            description: "Get Followers on Instagram. Low Drop.",
-            price: "R20"
-        }
-
         
     ]
 };
