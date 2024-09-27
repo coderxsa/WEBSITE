bf8fbb43f3e70e5a62909e98550d86281fdc03f9
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-09-27 12:52:45 +0200

    Update config.js

--- assets/servies/config.js
+++ assets/servies/config.js
@@ -17,20 +17,17 @@ const config = {
                 {
                     name: "Instagram Likes (1.5k)",
                     description: "Get real Likes on Instagram.",
-                    price: "R15",
-                    credit: 400
+                    price: "R17"
                 },
                 {
                     name: "Instagram Likes (10k)",
                     description: "Get real Likes on Instagram.",
-                    price: "R100",
-                    credit: 400
+                    price: "R110"
                 },
                 {
                     name: "Instagram Followers (500)",
                     description: "Get Followers on Instagram. Low Drop.",
-                    price: "R20",
-                    credit: 400
+                    price: "R22"
                 }
             ]
         },
@@ -40,68 +37,57 @@ const config = {
                 {
                     name: "Tiktok Views[bot] 3k - Free limited",
                     description: "Boost your TikTok views. Order List",
-                    price: "R0",
-                    credit: 400
+                    price: "R0"
                 },
                                 {
                     name: "Tiktok Views[bot] (100k)",
                     description: "Boost your TikTok views. Order List",
-                    price: "R15",
-                    credit: 400
+                    price: "R17"
                 },
                 {
                     name: "TikTok Likes (1k)",
                     description: "Increase your TikTok likes.",
-                    price: "R17",
-                    credit: 400
+                    price: "R19"
                 },
                 {
                     name: "TikTok Likes (10k)",
                     description: "Increase your TikTok likes.",
-                    price: "R100",
-                    credit: 400
+                    price: "R110"
                 },
                 {
                     name: "TikTok Shares (10k)",
                     description: "Increase your TikTok Shares.",
-                    price: "R15",
-                    credit: 400
+                    price: "R18"
                 },
                 {
                     name: "TikTok Shares (100k)",
                     description: "Increase your TikTok Shares.",
-                    price: "R22",
-                    credit: 400
+                    price: "R24"
                 },
                 {
                     name: "TikTok Saves (10k)",
                     description: "Increase your TikTok Saves.",
-                    price: "R15",
-                    credit: 400
+                    price: "R17"
                 },
                 {
                     name: "TikTok Saves (100k)",
                     description: "Increase your TikTok Saves.",
-                    price: "R22",
-                    credit: 400
+                    price: "R24"
                 },
                 {
                     name: "TikTok Followers (500)",
                     description: "Increase your TikTok Followers.",
-                    price: "R24",
-                    credit: 400
+                    price: "R26"
                 },
                 {
                     name: "TikTok Followers (1000)",
                     description: "Increase your TikTok Followers.",
-                    price: "R50",
-                    credit: 400
+                    price: "R52"
                 },
                 {
                     name: "TikTok Followers (4000)",
                     description: "Increase your TikTok Followers.",
-                    price: "R200",
-                    credit: 400
+                    price: "R210"
                 }
             ]
         },
